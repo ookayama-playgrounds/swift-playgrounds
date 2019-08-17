61e410f14ecd551ca65010bb26a59a36d76fdb1e
Riku Takano <rick.takano@gmail.com>
2019-08-17 20:47:03 +0900

    add readme about this project

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Riku Takano on 2019/08/17.
+//  Copyright © 2019 ookayama-playgrounds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FoodTracker/README.md
@@ -0,0 +1,8 @@
+# FoodTracker 
+
+これがチュートリアルとしてわかりやすい
+https://developer.apple.com/library/archive/referencelibrary/GettingStarted/DevelopiOSAppsSwift/BuildABasicUI.html#//apple_ref/doc/uid/TP40015214-CH5-SW1
+
+完成したら master にマージします。
+ぜひ手元で試してみてね。
+
